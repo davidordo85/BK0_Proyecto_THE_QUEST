c4ff311d6ea1d233528b11c1755eb6ff32a404ff
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-14 21:21:16 +0200

    Colocación de Nave

--- main.py
+++ main.py
@@ -3,10 +3,33 @@ from pygame.locals import *
 import sys
 
 BACKGROUND = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+class Nave:
+    def __init__(self):
+        self.vx = 5
+        self.vy = 5
+        self.x = 30
+        self.y = 300
+        self.h = 40
+        self.w = 25
+        self.centerx = self.x - self.w // 2
+
+        self.image = pg.Surface((40, 25))
+        self.image.fill(WHITE)
+
+
+
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode((800, 600))
         self.pantalla.fill(BACKGROUND)
+        espacio = pg.image.load("./resources/images/espacio.png")
+        self.pantalla.blit(espacio, (0, 0))
+        self.nave = Nave()
+        
+
+
         pg.display.set_caption("THE QUEST")
 
     def main_loop(self):
@@ -17,6 +40,7 @@ class Game:
                 if event.type == QUIT:
                     game_over = True
 
+            self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
             pg.display.flip()
 
     def quit(self):
