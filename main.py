5236b1ac2917f3444727b7a1421360fb2ddcc7a3
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-14 20:09:38 +0200

    Inicio del bucle

---
+++ main.py
@@ -0,0 +1,32 @@
+import pygame as pg
+from pygame.locals import *
+import sys
+
+BACKGROUND = (0, 0, 0)
+class Game:
+    def __init__(self):
+        self.pantalla = pg.display.set_mode((800, 600))
+        self.pantalla.fill(BACKGROUND)
+        pg.display.set_caption("THE QUEST")
+
+    def main_loop(self):
+        game_over = False
+
+        while not game_over:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    game_over = True
+
+            pg.display.flip()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+        
+if __name__ == '__main__':
+    pg.init()
+    game = Game()
+    game.main_loop()
+    game.quit()
+    
