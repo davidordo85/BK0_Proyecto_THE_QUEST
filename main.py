deaaef1bb255e412047c1b8747e7e9ec70e4506d
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-17 13:51:16 +0200

    Añadiendo asteroide con movimiento

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.locals import *
+from random import randint
 import sys
 
 BACKGROUND = (0, 0, 0)
@@ -9,26 +10,61 @@ class Nave:
     def __init__(self):
         self.vx = 5
         self.vy = 5
-        self.x = 30
-        self.y = 300
-        self.h = 40
-        self.w = 25
-        self.centerx = self.x - self.w // 2
+        self.Cx = 30
+        self.Cy = 300
+        self.h = 42
+        self.w = 45
+    
+        self.image = pg.image.load("./resources/images/mini_nave.png")
+        #self.image.fill()    
+    
+    @property
+    def posx(self):
+        return self.Cx - self.w // 2
+    @property
+    def posy(self):
+        return self.Cy - self.h // 2
 
-        self.image = pg.Surface((40, 25))
-        self.image.fill(WHITE)
 
+    def move(self, limSupY):
+        if self.Cy < self.h //2:
+            self.Cy = self.h // 2
+
+        if self.Cy > limSupY - self.h // 2:
+            self.Cy = limSupY - self.h // 2      
+
+class Asteroid:
+    def __init__(self):
+
+        self.vx = 3
+        self.vy = 0
+        self.Cx = 770
+        self.Cy = randint (30, 597)       
+        self.w = 30
+        self.h = 30
+
+        self.image = pg.image.load("./resources/images/asteroide.png")
+        #self.image.fill(WHITE)
+
+    @property
+    def posx(self):
+        return self.Cx - self.w // 2
+    @property
+    def posy(self):
+        return self.Cy - self.h // 2
+
+    
+    
 
 
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode((800, 600))
         self.pantalla.fill(BACKGROUND)
-        espacio = pg.image.load("./resources/images/espacio.png")
-        self.pantalla.blit(espacio, (0, 0))
-        self.nave = Nave()
-        
+        self.espacio = pg.image.load("./resources/images/espacio.png")
 
+        self.nave = Nave()
+        self.asteroid = Asteroid()
 
         pg.display.set_caption("THE QUEST")
 
@@ -40,8 +76,27 @@ class Game:
                 if event.type == QUIT:
                     game_over = True
 
-            self.pantalla.blit(self.nave.image, (self.nave.x, self.nave.y))
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        self.nave.Cy -= self.nave.vy
+                        
+                    if event.key == K_DOWN:
+                        self.nave.Cy += self.nave.vy
+            key_pressed = pg.key.get_pressed()
+            if key_pressed[K_UP]:
+                self.nave.Cy -= self.nave.vy
+            if key_pressed[K_DOWN]:
+                self.nave.Cy += self.nave.vy
+
+            self.pantalla.blit(self.espacio, (0, 0))
+            self.pantalla.blit(self.nave.image, (self.nave.posx, self.nave.posy))
+            self.pantalla.blit(self.asteroid.image, (self.asteroid.posx, self.asteroid.posy))
+            self.asteroid.Cx -= self.asteroid.vx
+
+            
+            self.nave.move(600)            
             pg.display.flip()
+            
 
     def quit(self):
         pg.quit()
