0eb288d49a4dbb35fe0c7eb9f94bc0cf4a179590
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-19 21:10:51 +0200

    comprobado el choque

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ WHITE = (255, 255, 255)
 
 class Nave:
     def __init__(self):
-        self.vx = 5
-        self.vy = 5
+        self.vx = 0
+        self.vy = 0
         self.Cx = 30
         self.Cy = 300
         self.h = 42
@@ -26,22 +26,35 @@ class Nave:
         return self.Cy - self.h // 2
 
 
-    def move(self, limSupY):
+    def move(self, limSupX, limSupY):
+        self.Cx += self.vx
+        self.Cy += self.vy
+
         if self.Cy < self.h //2:
             self.Cy = self.h // 2
 
         if self.Cy > limSupY - self.h // 2:
-            self.Cy = limSupY - self.h // 2      
+            self.Cy = limSupY - self.h // 2
+
+    def estrellado(self, something):
+        dx = abs(self.Cx - something.Cx)
+        dy = abs(self.Cy - something.Cy)
+
+        if dx < (self.w + something.w )//2 and dy < (self.h + something.h) // 2:
+            print("Estrellado") # quitar vida
+        else:
+            print("No estrellado") # game_over False
+
 
 class Asteroid:
     def __init__(self):
 
-        self.vx = 3
+        self.vx = randint (3, 5)
         self.vy = 0
         self.Cx = 770
         self.Cy = randint (30, 597)       
-        self.w = 30
-        self.h = 30
+        self.w = randint (30, 100) 
+        self.h = randint (30, 100)
 
 
         self.image = pg.image.load("./resources/images/asteroide.png")
@@ -68,7 +81,8 @@ class Game:
         self.espacio = pg.image.load("./resources/images/espacio.png")
 
         self.nave = Nave()
-        self.asteroid = Asteroid()
+        self.asteroid1 = Asteroid()
+        
 
         pg.display.set_caption("THE QUEST")
 
@@ -78,30 +92,37 @@ class Game:
         while not game_over:
             for event in pg.event.get():
                 if event.type == QUIT:
-                    game_over = True
-
+                    game_over = True                
+                '''
                 if event.type == KEYDOWN:
                     if event.key == K_UP:
-                        self.nave.Cy -= self.nave.vy
+                        self.nave.vy  = -5                        
                         
                     if event.key == K_DOWN:
-                        self.nave.Cy += self.nave.vy
+                        self.nave.vy  = 5
+                        '''
+
             key_pressed = pg.key.get_pressed()
             if key_pressed[K_UP]:
-                self.nave.Cy -= self.nave.vy
-            if key_pressed[K_DOWN]:
-                self.nave.Cy += self.nave.vy
-
-            
+                self.nave.vy = -5
+                
+            elif key_pressed[K_DOWN]:
+                self.nave.vy = 5
+            else:
+                self.nave.vy = 0
+                
 
 
             self.pantalla.blit(self.espacio, (0, 0))
             self.pantalla.blit(self.nave.image, (self.nave.posx, self.nave.posy))
-            self.pantalla.blit(self.asteroid.image, (self.asteroid.posx, self.asteroid.posy))          
+            self.pantalla.blit(self.asteroid1.image, (self.asteroid1.posx, self.asteroid1.posy))
+            
 
             
-            self.nave.move(600)
-            self.asteroid.move(0, 800)            
+            self.nave.move(800, 600)
+            self.asteroid1.move(0, 800)
+            self.nave.estrellado(self.asteroid1)            
+
             pg.display.flip()
             
 
@@ -115,4 +136,3 @@ if __name__ == '__main__':
     game = Game()
     game.main_loop()
     game.quit()
-    
