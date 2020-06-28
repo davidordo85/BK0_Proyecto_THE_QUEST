bf3eddd779de3c6939a37a7e64670f96b7cf87a0
davidordo85 <63961231+davidordo85@users.noreply.github.com>
2020-06-28 09:05:36 +0200

    Apunto de añadir los sprites

--- probatina_inicio de juego.py
+++ probatina_inicio de juego.py
@@ -2,82 +2,12 @@ import pygame as pg
 from pygame.locals import *
 from random import randint
 import sys
-
+from entities import *
 
 WHITE = (255,255,255)
 BLUE = (0,255,0)
 BLACK = (0, 0, 0)
-WIN_SCORE_GAME = 100
-
-class Nave:
-    def __init__(self):
-        self.vx = 0
-        self.vy = 0
-        self.Cx = 30
-        self.Cy = 300
-        self.h = 60
-        self.w = 25
-
-        self.image = pg.image.load('./resources/images/Rocket11_poquet.png')
-        self.destroy = pg.mixer.Sound('./resources/sounds/retro-explosion-07.wav')
-
-    @property
-    def posx(self):
-        return self.Cx - self.w // 2
-    @property
-    def posy(self):
-        return self.Cy - self.h // 2
-
-
-    def move(self, limSupX, limSupY):
-        self.Cx += self.vx
-        self.Cy += self.vy
-
-        if self.Cy < self.h //2:
-            self.Cy = self.h // 2
-
-        if self.Cy > limSupY - self.h // 2:
-            self.Cy = limSupY - self.h // 2
-
-    def estrellado(self, something):
-        dx = abs(self.Cx - something.Cx)
-        dy = abs(self.Cy - something.Cy)        
-
-        if dx < (self.w + something.w )//2 and dy < (self.h + something.h) // 2:
-            # quitar vida
-            self.destroy.play()            
-        else:
-            #print("No estrellado") # game_over False
-            pass
-
-class Asteroid:
-    def __init__(self):
-        self.vx = randint (3, 5)
-        self.vy = 0
-        self.Cx = 770
-        self.Cy = randint (30, 597)       
-        self.w = randint (30, 100) 
-        self.h = randint (30, 100)
-
-
-        self.image = pg.image.load("./resources/images/asteroide.png")
-        #self.image.fill(WHITE)
-
-    @property
-    def posx(self):
-        return self.Cx - self.w // 2
-    @property
-    def posy(self):
-        return self.Cy - self.h // 2
-
-    def move(self, limInfX, limSupX):
-        if self.Cx <= limInfX:
-            self.Cx = limSupX
-            self.Cy = randint (30, 597)
-
-        self.Cx -= self.vx
-
-
+WIN_SCORE_GAME = 1000
 
 class Game:
     def __init__(self):
@@ -108,7 +38,7 @@ class Game:
         self.font = pg.font.Font('./resources/fonts/lobster-Regular.ttf', 30)
         self.puntuacion = self.font.render("0", False, WHITE)
         self.nave = Nave()
-        self.asteroid1 = Asteroid()
+        self.asteroid = Asteroid()
 
         self.score = 0
 
@@ -138,7 +68,7 @@ class Game:
                 if event.type == KEYDOWN:
                     if event.key == K_SPACE:
                         inicio_portada = True
-
+            
 
             self.pantalla.fill((0, 0, 255))            
             self.pantalla.blit(self.text_inicial0, (30, 30))
@@ -147,6 +77,7 @@ class Game:
             self.pantalla.blit(self.text_inicial3, (30, 180))
 
             pg.display.flip()
+            #pg.time.delay( 10 )
 
         self.status = 'Partida'
 
@@ -154,51 +85,52 @@ class Game:
         for event in pg.event.get():            
             if event.type == QUIT:
                 return True
-                '''
+                
                 if event.type == KEYDOWN:
                     if event.key == K_UP:
                         self.nave.vy  = -5                        
                         
                     if event.key == K_DOWN:
                         self.nave.vy  = 5
-                        '''
+                        
 
         key_pressed = pg.key.get_pressed()
         if key_pressed[K_UP]:
-            self.nave.vy = -5
+            self.nave.vy -= 1
                 
         elif key_pressed[K_DOWN]:
-            self.nave.vy = 5
+            self.nave.vy += 1
         else:
             self.nave.vy = 0
 
         return False
         
     def bucle_partida(self):
-        game_over = False
+        bucle_juego = False
         self.score = 0
         self.puntuacion = self.font.render(str(self.score), True, WHITE)
-        while not game_over:
-            game_over = self.handlenEvent()            
+        while not bucle_juego:
+            bucle_juego = self.handlenEvent()            
                 
             self.nave.move(800, 600)
-            self.asteroid1.move(0, 800)
-            self.nave.estrellado(self.asteroid1)
+            self.asteroid.move(0, 770)
+            self.nave.estrellado(self.asteroid)
 
             
 
-            if self.asteroid1.Cx <= 0:                
+            if self.asteroid.Cx <= 0:                
                 self.score += 20
                 self.puntuacion = self.font.render(str(self.score), True, WHITE)
 
                 #print("Score {}".format(self.score))
 
                 if self.score == WIN_SCORE_GAME:
-                    game_over = True
+                    bucle_juego = True
+                    pg.quit()
                 
             self.pantalla.blit(self.espacio, (0, 0))
             self.pantalla.blit(self.nave.image, (self.nave.posx, self.nave.posy))
-            self.pantalla.blit(self.asteroid1.image, (self.asteroid1.posx, self.asteroid1.posy))
+            self.pantalla.blit(self.asteroid.image, (self.asteroid.posx, self.asteroid.posy))
             self.pantalla.blit(self.puntuacion, (740, 30))
             pg.display.flip()
 
